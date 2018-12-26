e6c89435e16586747596c5fae00c1e902ba7de30
itayyeka <itayyeka@gmail.com>
2018-12-26 14:56:08 +0200

    surf plot of both rErr and backoff

--- subFunctions/crlbSimpleSim.m
+++ subFunctions/crlbSimpleSim.m
@@ -11,6 +11,15 @@ D       = 0.01;
 theta   = pi/4; (this is where the poles are set)
 omega   = 2*pi*1e6;
 %}
+%% configure
+backoffFactor_min       = 0;
+backoffFactor_max       = 1;
+backoffFactor_nValues   = 10;
+rErr_min                = -20;
+rErr_max                = 20;
+rErr_nValues            = 30;
+
+%%
 
 simOutput = [];
 structToVariables(crlbSymbolics);
@@ -42,39 +51,56 @@ Jinv = inv(J);
 
 CRLB_theta = Jinv(1,1);
 
-%% creating the beta values
-syms                        x;
-polesThetaVec               = [pi/3 -pi/3]; 
-poles                       = exp(1i*omega*D*cos(polesThetaVec)/c); 
-polynomBeta                 = expand(prod(x-poles));
-betaV_ideal                 = coeffs(polynomBeta,x);
-phaseAlign                  = exp(1i*omega*tau);
-betaV_values_sym            = sym(zeros(size(betaV_ideal)));
-betaV_values_sym(1)         = (1-betaV_ideal(1))*phaseAlign;
-betaV_values_sym(2:end)     = -betaV_ideal(2:end)*phaseAlign;
-
 %% simulating
-alphaValues     = zeros(size(alphaV));
-alphaValues(1)  = 1;
-RangeVal        = 1000;
-omegaVal        = 2*pi*1e6;
-DVal            = 0.01;
-cVal            = 3e8;
-thetaVal        = polesThetaVec(1);
-
-symVarVec       = [D    ; omega     ; c     ; alphaV(:)      ; targetRange  ; theta      ];
-symVarValuesVec = [DVal ; omegaVal  ; cVal  ; alphaValues(:) ; RangeVal     ; thetaVal   ];
-
-CRLB_theta_kappa_sym    = subs(CRLB_theta,              symVarVec, symVarValuesVec);
-betaV_values            = reshape(eval(subs(betaV_values_sym,   symVarVec, symVarValuesVec)),size(betaV));
-
-pretty(CRLB_theta_kappa_sym)
-
-CRLB_theta_kappa    = [];
-kappaVec            = 0 : 0.01 : 1;
-for kappa = kappaVec
-    CRLB_theta_kappa(end+1)    = eval(subs(CRLB_theta_kappa_sym(end),  betaV, kappa*betaV_values));
+if true
+    %% creating the ideal beta values
+    syms                x;
+    polesThetaVec       = [pi/3 -pi/3];
+    poles               = exp(1i*omega*D*cos(polesThetaVec)/c);
+    polynomBeta         = expand(prod(x-poles));
+    betaV_ideal         = coeffs(polynomBeta,x);
+    phaseAlign          = exp(1i*omega*tau);
+    betaOpt_sym         = sym(zeros(size(betaV_ideal)));
+    betaOpt_sym(1)      = (1-betaV_ideal(1))*phaseAlign;
+    betaOpt_sym(2:end)  = -betaV_ideal(2:end)*phaseAlign;    
+    
+    %% setting constant parameter values
+    alphaValues     = zeros(size(alphaV));
+    alphaValues(1)  = 1;
+    RangeVal        = 1000;
+    omegaVal        = 2*pi*1e6;
+    DVal            = 0.01;
+    cVal            = 3e8;
+    thetaVal        = polesThetaVec(1);
+    
+    %% generating symbolic & values vec for easy substitution
+    symVarVec               = [D    ; omega     ; c     ; alphaV(:)      ; targetRange      ; theta      ];
+    symVarValuesVecIdeal    = [DVal ; omegaVal  ; cVal  ; alphaValues(:) ; RangeVal         ; thetaVal   ];
+    syms                    rErr;
+    symVarValuesVec         = [DVal ; omegaVal  ; cVal  ; alphaValues(:) ; RangeVal + rErr  ; thetaVal   ];
+        
+    %% calculating CRLB values
+    betaOptIdeal        = eval(subs(betaOpt_sym,  symVarVec,  symVarValuesVecIdeal));
+    CRLB_theta_sym      = subs(CRLB_theta,  symVarVec,  symVarValuesVec);    
+    backoffFactorVec    = linspace(backoffFactor_min    ,backoffFactor_max  ,backoffFactor_nValues);
+    rErrVec             = linspace(rErr_min             ,rErr_max           ,rErr_nValues);
+    
+    [backoffFactorMat, rErrMat] = meshgrid(backoffFactorVec, rErrVec);
+    crlbResultMat               = zeros(size(backoffFactorMat));
+    dynamicPrmSet               = [betaV(:) ; rErr];
+    
+    for paramSetID = 1:numel(crlbResultMat)
+        crlbResultMat(paramSetID)   = ...
+            subs(...
+            CRLB_theta_sym, ...
+            dynamicPrmSet, ...
+            [backoffFactorMat(paramSetID)*betaOptIdeal(:) ; rErrMat(paramSetID)] ...
+            ) ...
+            ;
+    end
+    
+    crlbResultMat   = eval('crlbResultMat;');
+    CRLB_theta_norm = crlbResultMat / max(abs(crlbResultMat(:)));
 end
-CRLB_theta_norm = CRLB_theta_kappa ;%/ max(abs(CRLB_theta_kappa));
-figure;plot(kappaVec,abs(CRLB_theta_norm(:)));
+figure;surf(backoffFactorMat, rErrMat, db(abs(CRLB_theta_norm)));
 end
