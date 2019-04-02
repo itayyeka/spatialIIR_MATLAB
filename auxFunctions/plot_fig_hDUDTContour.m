15ed7f6b5d94120f0b5018772e3b467d1d45c85c
itayyeka <itayyeka@gmail.com>
2019-04-02 07:40:17 +0300

    Update plot_fig_hDUDTContour.m

--- auxFunctions/plot_fig_hDUDTContour.m
+++ auxFunctions/plot_fig_hDUDTContour.m
@@ -9,8 +9,8 @@ dPhiVec     = rangeErrVec*pi/0.5;
 thetaVec    = linspace(0,pi,nPoints_sim);
 duVec       = pi*(cos(thetaVec)-cos(defaultCfg.thetaS));
 
-[dPhi_mat, du_mat]          = meshgrid(dPhiVec, duVec);
-[rangeErr_mat, dTheta_mat]  = meshgrid(rangeErrVec, thetaVec);
+[du_mat, dPhi_mat]          = meshgrid(duVec, dPhiVec);
+[dTheta_mat, rangeErr_mat]  = meshgrid(thetaVec, rangeErrVec);
 
 syms                dPhi DU N r;
 hAbs_symMAT         = subs(hAbs2Rel,{dPhi DU N},{dPhi_mat du_mat defaultCfg.nSensors});
@@ -19,27 +19,27 @@ if true
     %% r = 0
     hAbs_r0     = eval(subs(hAbs_symMAT,{r},{0}));
     figure; 
-    contourf(dPhi_mat/pi, dTheta_mat/pi, db(abs(hAbs_r0).^2), 'LevelList', contourLevelList);
-    colormap jet;
+    contourf(dTheta_mat/pi, dPhi_mat/pi, db(abs(hAbs_r0).^2), 'LevelList', contourLevelList);
+    colormap gray;
     %% r = 0.4
     hAbs_r04    = eval(subs(hAbs_symMAT,{r},{0.4}));
     figure; 
-    contourf(dPhi_mat/pi, dTheta_mat/pi, db(abs(hAbs_r04).^2), 'LevelList', contourLevelList);
-    colormap jet;
+    contourf(dTheta_mat/pi, dPhi_mat/pi, db(abs(hAbs_r04).^2), 'LevelList', contourLevelList);
+    colormap gray;
     %% r = 0.6
     hAbs_r06    = eval(subs(hAbs_symMAT,{r},{0.6}));
     figure; 
-    contourf(dPhi_mat/pi, dTheta_mat/pi, db(abs(hAbs_r06).^2), 'LevelList', contourLevelList);
-    colormap jet;
+    contourf(dTheta_mat/pi, dPhi_mat/pi, db(abs(hAbs_r06).^2), 'LevelList', contourLevelList);
+    colormap gray;    
     %% r = 0.8
     hAbs_r08    = eval(subs(hAbs_symMAT,{r},{0.8}));
     figure; 
-    contourf(dPhi_mat/pi, dTheta_mat/pi, db(abs(hAbs_r08).^2), 'LevelList', contourLevelList);
-    colormap jet;
+    contourf(dTheta_mat/pi, dPhi_mat/pi, db(abs(hAbs_r08).^2), 'LevelList', contourLevelList);
+    colormap gray;
     %% r = 0.9
     hAbs_r09    = eval(subs(hAbs_symMAT,{r},{0.9}));
     figure; 
-    contourf(dPhi_mat/pi, dTheta_mat/pi, db(abs(hAbs_r09).^2), 'LevelList', contourLevelList);
-    colormap jet;
+    contourf(dTheta_mat/pi, dPhi_mat/pi, db(abs(hAbs_r09).^2), 'LevelList', contourLevelList);
+    colormap gray;
 end
 end
