baeabaa9c0a825b4aaa47367151a6ecef9efa2da
itayyeka <itayyeka@gmail.com>
2019-08-06 00:33:25 +0300

    Plotting now the fig_directivity

--- auxFunctions/directivityAnalysis.m
+++ auxFunctions/directivityAnalysis.m
@@ -7,10 +7,10 @@ catch
     clc;
 end
 %% configure
-nVec    = 2:7;
+nVec    = 2:10;
 rVec    = [0.25 : 0.05 : 0.75];
 rVec    = [0.2 : 0.1 : 0.7];
-rVec    = [1/10 1/9 1/8 1/7 1/6 1/5 1/4 1/3 1/2];
+rVec    = [1/10 1/5 1/3 1/2 5/8 3/4 4/5];
 
 %% symbolics
 syms x real;
@@ -25,6 +25,7 @@ h = @(t,p,N,r) hNum(t,p,N,r)./hDen(t,p,N,r);
 %% simulate
 integralResultMAT   = zeros(length(rVec),length(nVec));
 exprMAT             = zeros(size(integralResultMAT));
+[r_GRID, n_GRID]    = meshgrid(rVec, nVec);
 for rVal = rVec
     for nVal = nVec    
         curIntRes = ...
@@ -38,7 +39,6 @@ for rVal = rVec
         Dir_eval = eval(Dir);
         Dir_STR = rats(real(Dir_eval));
         tol = abs(eval(Dir_STR)-Dir_eval);
-        expr = (rVal^2-(nVal-1)*rVal+nVal)/((1-rVal)^2);
         disp([...
             'r = ', num2str(rVal) ...
             ,', N = ', num2str(nVal) ...
@@ -46,7 +46,6 @@ for rVal = rVec
             ,', Dir = ', Dir_STR ...
             ...,', Dir(1-r)^2 = ', rats(eval(Dir_STR)*(1-rVal)^2) ...
             ...,', tol = ', num2str(tol) ...
-            ', Expr = ', rats(expr), ...
             ]);
         integralResultMAT(...
             rVec == rVal,...
@@ -58,11 +57,16 @@ for rVal = rVec
             ) = (nVal-rVal)/(1-rVal);
     end    
 end
-errMAT = abs(exprMAT - integralResultMAT);
-integralResultMAT_CELL = num2cell(integralResultMAT);
-integralResultMAT_CELL_rat = cellfun(@(x) rats(x), integralResultMAT_CELL, 'UniformOutput', false);
-rVec_CELL = num2cell(rVec(:));
-rVec_CELL_STR = cellfun(@(x) rats(x), rVec_CELL, 'UniformOutput', false);
-nVec_CELL_STR = cellfun(@(x) ['N' num2str(x)], num2cell(nVec), 'UniformOutput', false);
-T = array2table([rVec_CELL_STR(:) integralResultMAT_CELL_rat],...
-    'VariableNames',[{'r'} reshape(nVec_CELL_STR,1,[])])
+figure;
+surf(r_GRID, n_GRID, transpose(integralResultMAT), 'EdgeColor', "interp", 'FaceAlpha',0.75);
+hold on;
+plot3(r_GRID, n_GRID, transpose(exprMAT), 'dk', 'MarkerSize', 10, 'LineWidth', 2, 'MarkerFaceColor', 'k');
+legend({'Numeric integration', 'Analytic expression'});
+% errMAT = abs(exprMAT - integralResultMAT)
+% integralResultMAT_CELL = num2cell(integralResultMAT);
+% integralResultMAT_CELL_rat = cellfun(@(x) rats(x), integralResultMAT_CELL, 'UniformOutput', false);
+% rVec_CELL = num2cell(rVec(:));
+% rVec_CELL_STR = cellfun(@(x) rats(x), rVec_CELL, 'UniformOutput', false);
+% nVec_CELL_STR = cellfun(@(x) ['N' num2str(x)], num2cell(nVec), 'UniformOutput', false);
+% T = array2table([rVec_CELL_STR(:) integralResultMAT_CELL_rat],...
+%     'VariableNames',[{'r'} reshape(nVec_CELL_STR,1,[])])
